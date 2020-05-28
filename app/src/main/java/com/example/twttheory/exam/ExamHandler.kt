ad2fa041e6b85879f02108f4e63ebf07c814ea30
cjdjczym <1732018999@qq.com>
2020-05-28 19:23:05 +0800

    数据类改动

--- app/src/main/java/com/example/twttheory/exam/ExamHandler.kt
+++ app/src/main/java/com/example/twttheory/exam/ExamHandler.kt
@@ -2,13 +2,13 @@ package com.example.twttheory.exam
 
 import com.example.twttheory.service.RefreshState
 import com.example.twttheory.service.awaitAndHandle
+import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import okhttp3.MediaType
 import okhttp3.RequestBody
-
-//TODO：Model完善（更新recyclerView）
+import java.util.stream.Collectors
 
 fun generateRequestBody(requestDataMap: Map<String, String>): Map<String, RequestBody> {
     val requestBodyMap: MutableMap<String, RequestBody> = HashMap()
@@ -21,6 +21,37 @@ fun generateRequestBody(requestDataMap: Map<String, String>): Map<String, Reques
         requestBodyMap[key] = requestBody
     }
     return requestBodyMap
+}
+
+//测试用
+fun testFun(
+    callback: suspend (RefreshState<String>) -> Unit = {}
+) {
+    GlobalScope.launch(Dispatchers.Main) {
+        val test1 = listOf(0, 1, 2)
+        val test2 = listOf(3, 4, 5)
+        val map = mutableMapOf<String, String>()
+        for (i in test1.indices) map["test1[$i]"] = test1[i].toString()
+        for (i in test2.indices) map["test2[$i]"] = test2[i].toString()
+        val params = mapOf(
+            "test1" to test1,
+            "test2" to test2,
+            "test3" to mapOf(
+                "test3_1" to test1,
+                "test3_2" to test2
+            )
+        )
+        ExamService.test(
+            RequestBody.create(
+                MediaType.parse("application/json;charset=UTF-8"),
+                Gson().toJson(params)
+            )
+        ).awaitAndHandle {
+            callback(RefreshState.Failure(it))
+        }?.data?.let {
+            callback(RefreshState.Success(it.test3.toString()))
+        }
+    }
 }
 
 //创建新任务
@@ -38,27 +69,34 @@ fun createExam(
     times: Int,
     callback: suspend (RefreshState<Unit>) -> Unit = {}
 ) {
-    GlobalScope.launch(Dispatchers.Main) {
-        val params = mapOf(
-            "paper_type" to paper_type.toString(),
-            "paper_name" to paper_name,
-            "paper_hint" to paper_hint,
-            "number" to number.toString(),
-            "is_random" to is_random.toString(),
-            "paper_question" to paper_question.toString(),
-            "start_time" to start_time,
-            "end_time" to end_time,
-            "last_time" to last_time.toString(),
-            "password" to password,
-            "times" to times.toString()
-        )
-        ExamService.createTask(generateRequestBody(params)).awaitAndHandle {
-            callback(RefreshState.Failure(it))
-        }?.data?.let {
-            if (it) callback(RefreshState.Success(Unit))
-            else callback(RefreshState.Error(Unit))
-        }
-    }
+//    GlobalScope.launch(Dispatchers.Main) {
+//        val map = mutableMapOf<String, Any>(
+//            "paper_type" to paper_type.toString(),
+//            "paper_name" to paper_name,
+//            "paper_hint" to paper_hint,
+//            "number" to number.toString(),
+//            "is_random" to is_random.toString(),
+////            "paper_question" to paper_question.toString(),
+//            "start_time" to start_time,
+//            "end_time" to end_time,
+//            "last_time" to last_time.toString(),
+//            "password" to password,
+//            "times" to times.toString()
+//        )
+//        val answerList:List<Map<String,String>>
+//        for (i in paper_question.indices) map["paper_question[$i]"] = mapOf(
+//            "question" to paper_question[i].question,
+//            "answer" to mutableMapOf<String, String>()
+//        )
+//
+//        for (i in paper_question.indices)
+//        ExamService.createTask(params).awaitAndHandle {
+//            callback(RefreshState.Failure(it))
+//        }?.data?.let {
+//            if (it) callback(RefreshState.Success(Unit))
+//            else callback(RefreshState.Error(Unit))
+//        }
+//    }
 }
 
 //暂停任务
@@ -66,19 +104,19 @@ fun pauseExam(
     paper_id: Int,
     callback: suspend (RefreshState<Unit>) -> Unit = {}
 ) {
-    GlobalScope.launch(Dispatchers.Main) {
-        ExamService.pauseTask(
-            RequestBody.create(
-                MediaType.parse("multipart/form-data"),
-                paper_id.toString()
-            )
-        ).awaitAndHandle {
-            callback(RefreshState.Failure(it))
-        }?.data?.let {
-            if (it) callback(RefreshState.Success(Unit))
-            else callback(RefreshState.Error(Unit))
-        }
-    }
+//    GlobalScope.launch(Dispatchers.Main) {
+//        ExamService.pauseTask(
+//            RequestBody.create(
+//                MediaType.parse("multipart/form-data"),
+//                paper_id.toString()
+//            )
+//        ).awaitAndHandle {
+//            callback(RefreshState.Failure(it))
+//        }?.data?.let {
+//            if (it) callback(RefreshState.Success(Unit))
+//            else callback(RefreshState.Error(Unit))
+//        }
+//    }
 }
 
 //修改任务限制
@@ -93,24 +131,24 @@ fun changeLimit(
     times: String,
     callback: suspend (RefreshState<Unit>) -> Unit = {}
 ) {
-    GlobalScope.launch(Dispatchers.Main) {
-        val params = mapOf(
-            "paper_id" to paper_id.toString(),
-            "paper_name" to paper_name,
-            "paper_hint" to paper_hint,
-            "start_time" to start_time,
-            "end_time" to end_time,
-            "last_time" to last_time,
-            "password" to password,
-            "times" to times
-        )
-        ExamService.changeLimit(generateRequestBody(params)).awaitAndHandle {
-            callback(RefreshState.Failure(it))
-        }?.data?.let {
-            if (it) callback(RefreshState.Success(Unit))
-            else callback(RefreshState.Error(Unit))
-        }
-    }
+//    GlobalScope.launch(Dispatchers.Main) {
+//        val params = mapOf(
+//            "paper_id" to paper_id.toString(),
+//            "paper_name" to paper_name,
+//            "paper_hint" to paper_hint,
+//            "start_time" to start_time,
+//            "end_time" to end_time,
+//            "last_time" to last_time,
+//            "password" to password,
+//            "times" to times
+//        )
+//        ExamService.changeLimit(generateRequestBody(params)).awaitAndHandle {
+//            callback(RefreshState.Failure(it))
+//        }?.data?.let {
+//            if (it) callback(RefreshState.Success(Unit))
+//            else callback(RefreshState.Error(Unit))
+//        }
+//    }
 }
 
 //下载问卷
@@ -118,19 +156,19 @@ fun downloadExam(
     paper_id: Int,
     callback: suspend (RefreshState<Unit>) -> Unit = {}
 ) {
-    GlobalScope.launch(Dispatchers.Main) {
-        ExamService.downloadTask(
-            RequestBody.create(
-                MediaType.parse("multipart/form-data"),
-                paper_id.toString()
-            )
-        ).awaitAndHandle {
-            callback(RefreshState.Failure(it))
-        }?.data?.let {
-            //具体下载操作还没实现!!!!!!!!!!!!!
-            callback(RefreshState.Success(Unit))
-        }
-    }
+//    GlobalScope.launch(Dispatchers.Main) {
+//        ExamService.downloadTask(
+//            RequestBody.create(
+//                MediaType.parse("multipart/form-data"),
+//                paper_id.toString()
+//            )
+//        ).awaitAndHandle {
+//            callback(RefreshState.Failure(it))
+//        }?.data?.let {
+//            //具体下载操作还没实现!!!!!!!!!!!!!
+//            callback(RefreshState.Success(Unit))
+//        }
+//    }
 }
 
 //停止并删除问卷
@@ -138,19 +176,19 @@ fun deleteExam(
     paper_id: Int,
     callback: suspend (RefreshState<Unit>) -> Unit = {}
 ) {
-    GlobalScope.launch(Dispatchers.Main) {
-        ExamService.deleteTask(
-            RequestBody.create(
-                MediaType.parse("multipart/form-data"),
-                paper_id.toString()
-            )
-        ).awaitAndHandle {
-            callback(RefreshState.Failure(it))
-        }?.data?.let {
-            if (it) callback(RefreshState.Success(Unit))
-            else callback(RefreshState.Error(Unit))
-        }
-    }
+//    GlobalScope.launch(Dispatchers.Main) {
+//        ExamService.deleteTask(
+//            RequestBody.create(
+//                MediaType.parse("multipart/form-data"),
+//                paper_id.toString()
+//            )
+//        ).awaitAndHandle {
+//            callback(RefreshState.Failure(it))
+//        }?.data?.let {
+//            if (it) callback(RefreshState.Success(Unit))
+//            else callback(RefreshState.Error(Unit))
+//        }
+//    }
 }
 
 //获取修改题目
@@ -158,19 +196,19 @@ fun getChangeExam(
     paper_id: Int,
     callback: suspend (RefreshState<Unit>) -> Unit = {}
 ) {
-    GlobalScope.launch(Dispatchers.Main) {
-        ExamService.getChangeTask(
-            RequestBody.create(
-                MediaType.parse("multipart/form-data"),
-                paper_id.toString()
-            )
-        ).awaitAndHandle {
-            callback(RefreshState.Failure(it))
-        }?.data?.let {
-            ExamModel.updateChangedQuestion(it)
-            callback(RefreshState.Success(Unit))
-        }
-    }
+//    GlobalScope.launch(Dispatchers.Main) {
+//        ExamService.getChangeTask(
+//            RequestBody.create(
+//                MediaType.parse("multipart/form-data"),
+//                paper_id.toString()
+//            )
+//        ).awaitAndHandle {
+//            callback(RefreshState.Failure(it))
+//        }?.data?.let {
+//            ExamModel.updateChangedQuestion(it)
+//            callback(RefreshState.Success(Unit))
+//        }
+//    }
 }
 
 //提交修改
@@ -179,18 +217,18 @@ fun postChangeExam(
     paper_id: Int,
     callback: suspend (RefreshState<Unit>) -> Unit = {}
 ) {
-    GlobalScope.launch(Dispatchers.Main) {
-        val params = mapOf(
-            "question" to question.toString(),
-            "paper_id" to paper_id.toString()
-        )
-        ExamService.postChangeTask(generateRequestBody(params)).awaitAndHandle {
-            callback(RefreshState.Failure(it))
-        }?.data?.let {
-            if (it) callback(RefreshState.Success(Unit))
-            else callback(RefreshState.Error(Unit))
-        }
-    }
+//    GlobalScope.launch(Dispatchers.Main) {
+//        val params = mapOf(
+//            "question" to question.toString(),
+//            "paper_id" to paper_id.toString()
+//        )
+//        ExamService.postChangeTask(generateRequestBody(params)).awaitAndHandle {
+//            callback(RefreshState.Failure(it))
+//        }?.data?.let {
+//            if (it) callback(RefreshState.Success(Unit))
+//            else callback(RefreshState.Error(Unit))
+//        }
+//    }
 }
 
 
@@ -229,19 +267,19 @@ fun getExam(
     callback: suspend (RefreshState<Unit>) -> Unit = {}
 ) {
     GlobalScope.launch(Dispatchers.Main) {
-        ExamService.getTask(
-            RequestBody.create(
-                MediaType.parse("multipart/form-data"), paper_id.toString()
-            ),
-            RequestBody.create(
-                MediaType.parse("multipart/form-data"), password
-            )
-        ).awaitAndHandle {
-            callback(RefreshState.Failure(it))
-        }?.data?.let {
-            ExamModel.updateQuestions(it.paper_question)
-            callback(RefreshState.Success(Unit))
-        }
+//        ExamService.getTask(
+//            RequestBody.create(
+//                MediaType.parse("multipart/form-data"), paper_id.toString()
+//            ),
+//            RequestBody.create(
+//                MediaType.parse("multipart/form-data"), password
+//            )
+//        ).awaitAndHandle {
+//            callback(RefreshState.Failure(it))
+//        }?.data?.let {
+//            ExamModel.updateQuestions(it.paper_question)
+//            callback(RefreshState.Success(Unit))
+//        }
     }
 }
 
@@ -258,11 +296,11 @@ fun solveExam(
             "submit_id" to submit_id.toString(),
             "paper_question" to paper_question.toString()
         )
-        ExamService.solveTask(generateRequestBody(params)).awaitAndHandle {
-            callback(RefreshState.Failure(it))
-        }?.data?.let {
-            if (it) callback(RefreshState.Success(Unit))
-            else callback(RefreshState.Error(Unit))
-        }
+//        ExamService.solveTask(generateRequestBody(params)).awaitAndHandle {
+//            callback(RefreshState.Failure(it))
+//        }?.data?.let {
+//            if (it) callback(RefreshState.Success(Unit))
+//            else callback(RefreshState.Error(Unit))
+//        }
     }
 }
